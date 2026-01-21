2c8099f70e5b46de16d0aa0fbd675584decf7b8b
ncbolano <142913812+ncbolano@users.noreply.github.com>
2026-01-21 07:47:59 -0600

    sim function added with changes of k indexing to j , ks to k , and M to M_grid[j].

--- R/run_script.R
+++ R/run_script.R
@@ -24,8 +24,51 @@ M_grid = unique(round(seq(max(coefnum , n^(1/5)), n^(1/2), length.out = 50)))
 CV_scores = numeric(length(M_grid))
 M_list = local_M_selection(JJ, k, M_grid)
 
+# sim function
+n_k = length(k)
+betaCoefAll = array(0, c(n_k, coefnum, p))
+varbeta = array(0, c(n_k, coefnum, 2, p))
+varbetaAll = array(0, c(n_k, 2 * coefnum, 2 * coefnum, p))
 
+for (j in seq_along(k)) {
+  for (i_p in 1:p) {
+    betaCoefAll[j, , i_p] = beta(J, k[j], nu, Kernel_function, M_list[j], a = i_p, delta = 0)
+    varbetaAll[j, , , i_p] = variance.estimator.v2(J, k[j], nu, Kernel_function, M_list[j], a = i_p, L, delta = 0)
+    varbeta[j, , 1, i_p] = diag(varbetaAll[j, 1:coefnum, 1:coefnum, i_p])
+    varbeta[j, , 2, i_p] = diag(varbetaAll[j, 1:coefnum + coefnum, 1:coefnum + coefnum, i_p])
+  }
+}
+list(betaCoefAll, varbetaAll, varbeta)
 
+betaCoefAll = sapply(sim_Res[1, ], function(x) x, simplify = 'array') |> aperm(c(4, 1:3))
+varbetaAll = sapply(sim_Res[2, ], function(x) x, simplify = 'array') |> aperm(c(5, 1:4))
+varbeta = sapply(sim_Res[3, ], function(x) x, simplify = 'array') |> aperm(c(5, 1:4))
 
-
+Test_tibble = NULL
+for (a in 1:p) {
+  for (c in 1:p) {
+    for (r in 0:(nu)) {
+      for (j in seq_along(k)) {
+        if (!(a == c & r == 0)) {
+          loc = r_to_loc(r, c, a, nu = nu, p = p)
+          tmp1 = pnorm(abs(Re(betaCoefAll[, j, loc, a])) / sqrt(abs(varbeta[, j, loc, 1, a])), lower.tail = F) * 2
+          tmp2 = pnorm(abs(Im(betaCoefAll[, j, loc, a])) / sqrt(abs(varbeta[, j, loc, 2, a])), lower.tail = F) * 2
+          tmp3 = unlist(purrr::map2(tmp1, tmp2, function(x, y) min(p.adjust(c(x, y), method = "BY"))))
+          tmp4 = sapply(1:R, function(i) {
+            tmp1_vec = c(Re(betaCoefAll[i, j, loc, a]), Im(betaCoefAll[i, j, loc, a]))
+            matrix_tmp = varbetaAll[i, j, c(loc, loc + coefnum), c(loc, loc + coefnum), a]
+            matrix_tmp[1, 2] = 0  # Fixed indexing
+            matrix_tmp[2, 1] = 0  # Fixed indexing
+            matrix_tmp = abs(matrix_tmp)
+            return(pchisq(tmp1_vec %*% solve(matrix_tmp, b = tmp1_vec), lower.tail = F, df = 2))
+          })
+          Test_tibble = cbind(Re = tmp1, Im = tmp2, padjust = tmp3, Chisq = tmp4) |>
+            as_tibble() |>
+            mutate(a = a, c = c, r = r, k1 = k[j], i = 1:R) |>
+            rbind(Test_tibble)
+        }
+      }
+    }
+  }
+}
 
