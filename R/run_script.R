3c5b3c963da8c0e815a7134975becc724cfb3895
ncbolano <142913812+ncbolano@users.noreply.github.com>
2026-01-21 07:32:47 -0600

    Variable extraction + MK estimation added.

--- R/run_script.R
+++ R/run_script.R
@@ -9,5 +9,23 @@ source('VarianceFunctions.R')
 
 # Extract key variables from seq. x
 variable_list = extractVariables(x)
-JJ = variable_list[1] ; p = variable_list[2] ; nu = variable_list[3]
+J = variable_list[1] ; p = variable_list[2] ; nu = variable_list[3]
+coefnum = (2 * p * nu) + p - 1
+
+# Gathering frequencies (k) + local smoothing values (m_i) for each frequency
+k = extractK(J,coefnum)
+
+# End of chosen list of two k frequencies
+# Obtaining M_list for each k frequency
+M_list = local_M_selection(JJ, k, M_grid)
+M_grid = unique(round(seq(max(coefnum , n^(1/5)), n^(1/2), length.out = 50)))
+
+
+CV_scores = numeric(length(M_grid))
+M_list = local_M_selection(JJ, k, M_grid)
+
+
+
+
+
 
