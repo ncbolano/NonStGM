25ecfcbedc27a8c281ce1d9ade0d6e7c984f65eb
Noah Bolanos <ncbolano@tamu.edu>
2026-01-21 15:44:17 -0600

    Initialized

--- R/exampleVAR.R
+++ R/exampleVAR.R
@@ -1,3 +1,11 @@
+#' Transformation of frequencies (excluding a single index)
+#'
+#' @param J P-dimensional discrete fourier transform
+#' @param k scalar value for \omega(k) = (2pi *k*) / n
+#' @param nu scalar value which specifies amount of local DFT's to create frequency matrix *J_k^n*
+#' @param a scalar index value which determines removal of specific index from local DFT matrix
+#' @return
+#' @noRd
 sim.tvVAR = function(burnin, m, TV_size) {
   A = matrix(c(0.5, 0.2, 0, 0, 0.8, 0, 0, 0.3, 0.6), ncol = 3, byrow = T)
   n = m + burnin
@@ -14,3 +22,5 @@ sim.tvVAR = function(burnin, m, TV_size) {
   x2 = x1[-c(1:burnin), ]
   return(x2)
 }
+
+x = sim.tvVAR(200,2048,.6)
